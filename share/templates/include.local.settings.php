a530b79aa9f1ba465bb983cc355894e2ef22dc0d
Jamie McClelland <jm@mayfirst.org>
2015-06-10 16:04:36 -0400

    ensure we get our local.settings.php file in place.

---
+++ share/templates/include.local.settings.php
@@ -0,0 +1,6 @@
+
+# Additional site configuration settings.
+if (file_exists('/var/www/powerbase/sites/default/local.settings.php')) {
+  include_once('/var/www/powerbase/sites/default/local.settings.php');
+}
+
---
+++ share/templates/local.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+error_reporting(LONG_ERROR_REPORTING);
+global $civicrm_setting;
+$civicrm_setting["Extension Preferences"]["ext_repo_url"] = FALSE;
+$civicrm_setting["CiviCRM Preferences"]["communityMessagesUrl"] = FALSE;
+$civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_key"] = "LONG_YAHOO_BOSS_API_KEY";
+$civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_secret"] = "LONG_BOSS_API_SECRET";
+$civicrm_setting["Geocoding Preferences"]["path_to_oauth_library"] = "/usr/share/php/OAuth.php";
