a656a7c5f6568749c5ec91558f3d15296f1cf4ee
Jamie McClelland <jm@mayfirst.org>
2015-07-21 10:13:47 -0400

    adding custom fonts and better error reporting on container fail.

--- share/templates/local.settings.php
+++ share/templates/local.settings.php
@@ -7,3 +7,13 @@ $civicrm_setting["CiviCRM Preferences"]["communityMessagesUrl"] = FALSE;
 $civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_key"] = "LONG_YAHOO_BOSS_API_KEY";
 $civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_secret"] = "LONG_BOSS_API_SECRET";
 $civicrm_setting["Geocoding Preferences"]["path_to_oauth_library"] = "/usr/share/php/OAuth.php";
+
+// All sites have access to extra language fonts.
+define("K_PATH_FONTS", "/var/www/powerbase/sites/all/libraries/fonts/");
+$civicrm_setting["CiviCRM Preferences"]["additional_fonts"] = array(
+  "batang" => "Batang",
+  "dotum" => "Dotum",
+  "hline" => "Hline",
+  "gulim" => "Gulim",
+);
+
