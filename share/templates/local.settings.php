8a5478255f1fd28128e01275c32963167cc0c1e4
Jamie McClelland <jm@mayfirst.org>
2015-07-21 11:33:51 -0400

    fixing var names for yahoo geo coding.

--- share/templates/local.settings.php
+++ share/templates/local.settings.php
@@ -5,7 +5,7 @@ global $civicrm_setting;
 $civicrm_setting["Extension Preferences"]["ext_repo_url"] = FALSE;
 $civicrm_setting["CiviCRM Preferences"]["communityMessagesUrl"] = FALSE;
 $civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_key"] = "LONG_YAHOO_BOSS_API_KEY";
-$civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_secret"] = "LONG_BOSS_API_SECRET";
+$civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_secret"] = "LONG_YAHOO_BOSS_API_SECRET";
 $civicrm_setting["Geocoding Preferences"]["path_to_oauth_library"] = "/usr/share/php/OAuth.php";
 
 // All sites have access to extra language fonts.
