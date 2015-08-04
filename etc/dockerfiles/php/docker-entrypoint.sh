8d17a8d0c1ab9fe4e8094c7ebdbea67a3f8f0459
Jamie McClelland <jm@mayfirst.org>
2015-08-04 12:46:14 -0400

    add more debugging output on dev serveres.

--- etc/dockerfiles/php/docker-entrypoint.sh
+++ etc/dockerfiles/php/docker-entrypoint.sh
@@ -11,5 +11,13 @@ fi
 if [ "$1" = 'php5-fpm' ]; then
   set -- "$@" --nodaemonize
 fi
+# On dev servers we add more logging output 
+if [ "$LONG_LIVE" = "n" ]; then
+  printf "display_errors = On\n" > '/etc/php5/fpm/conf.d/99-powerbase-dev.ini'
+  printf "display_startup_errors = On\n" >> '/etc/php5/fpm/99-conf.d/powerbase-dev.ini'
+  printf "error_reporting = E_ALL\n" >> '/etc/php5/fpm/conf.d/99-powerbase-dev.ini'
+  printf "html_errors = On\n" >> '/etc/php5/fpm/conf.d/99-powerbase-dev.ini'
+  printf "log_errors = On\n" >> '/etc/php5/fpm/conf.d/99-powerbase-dev.ini'
+fi
 
 exec "$@"
