7b0868f3ae4863aaf4fb8fc47ab1c4cf5a0dda16
Jamie McClelland <jm@mayfirst.org>
2015-08-12 12:41:13 -0400

    ensure IE works properly with iframes.
    https://support.ourpowerbase.net/ticket/1575

--- share/templates/local.settings.php
+++ share/templates/local.settings.php
@@ -1,5 +1,11 @@
 <?php
 
+// This header is here so that IE users that are accessing a contribution or event
+// page via an iframe embedded in another web site, will get their cookie/session
+// settings set properly even though the iframe is pointing to a different domain
+// than the parent page. For more information on what these codes mean, see:
+// https://support.ourpowerbase.net/ticket/1575
+header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
 error_reporting(LONG_ERROR_REPORTING);
 global $civicrm_setting;
 $civicrm_setting["Extension Preferences"]["ext_repo_url"] = FALSE;
