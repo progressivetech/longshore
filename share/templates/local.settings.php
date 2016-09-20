ed1c6597dd393e3ae2d2316b89ddde49b9c8af55
Jamie McClelland <jm@mayfirst.org>
2016-09-20 11:41:32 -0400

    massive refactoring to improve handling of settings
    Also removes yahoo boss which was dis-continued.

--- share/templates/local.settings.php
+++ share/templates/local.settings.php
@@ -7,29 +7,13 @@
 // https://support.ourpowerbase.net/ticket/1575
 header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
 error_reporting(LONG_ERROR_REPORTING);
-global $civicrm_setting;
-$civicrm_setting["Extension Preferences"]["ext_repo_url"] = FALSE;
-$civicrm_setting["CiviCRM Preferences"]["communityMessagesUrl"] = FALSE;
-$civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_key"] = "LONG_YAHOO_BOSS_API_KEY";
-$civicrm_setting["Geocoding Preferences"]["yahoo_boss_api_secret"] = "LONG_YAHOO_BOSS_API_SECRET";
-$civicrm_setting["Geocoding Preferences"]["path_to_oauth_library"] = "/usr/share/php/OAuth.php";
 
 // All sites have access to extra language fonts.
 define("K_PATH_FONTS", "/var/www/powerbase/sites/all/libraries/fonts/");
-$civicrm_setting["CiviCRM Preferences"]["additional_fonts"] = array(
-  "batang" => "Batang",
-  "dotum" => "Dotum",
-  "hline" => "Hline",
-  "gulim" => "Gulim",
-);
 
 // If we are using stripe - be sure to use the PHP submission method
 // See: https://github.com/drastik/com.drastikbydesign.stripe/issues/113
 define('STRIPE_SUBMISSION_METHOD', 'php');
-
-// Turn off all security/update settings
-$civicrm_setting["CiviCRM Preferences"]["securityUpdateAlert"] = "1";
-$civicrm_setting["CiviCRM Preferences"]["versionAlert"] = "1";
 
 // Ensure pages can be embedded via iframes by disabling drupal protection
 $conf['x_frame_options'] = '';
