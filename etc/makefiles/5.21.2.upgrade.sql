c26348e050d380eea52498adb5c2d04be8557759
Jamie McClelland <jm@mayfirst.org>
2020-04-08 09:17:48 -0400

    contacttype entities also mess things up.

--- etc/makefiles/5.21.2.upgrade.sql
+++ etc/makefiles/5.21.2.upgrade.sql
@@ -1,5 +1,5 @@
 -- Create a sql file that can be imported back
-SELECT CONCAT("INSERT INTO civicrm_managed SET id = ", id, ",module='", module, "', name='", name, "', entity_type='PaymentProcessorType', entity_id=", entity_id, ";") FROM civicrm_managed WHERE entity_type = 'PaymentProcessorType';
+SELECT CONCAT("INSERT INTO civicrm_managed SET id = ", id, ",module='", module, "', name='", name, "', entity_type='", entity_type, "', entity_id=", entity_id, ";") FROM civicrm_managed WHERE entity_type = 'PaymentProcessorType' OR entity_type = 'ContactType';
 -- Now delete
 DELETE FROM civicrm_managed WHERE entity_type = 'PaymentProcessorType';
 
