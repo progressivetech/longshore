8e67dd1770a37230cd14ae758fd1d802d39f8860
Jamie McClelland <jm@mayfirst.org>
2021-08-16 15:03:10 -0400

    preparing for bullseye upgrade of containers.

--- etc/dockerfiles/php/docker-entrypoint.sh
+++ etc/dockerfiles/php/docker-entrypoint.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set -e
 
+php_version=7.4
 # If the command passed to docker run/exec starts with a - or -- then
 # set the command to php5-cgi and pass whatever follows as an argument to
 # php5-cgi.
 if [ "${1:0:1}" = '-' ]; then
-	set -- php-fpm7.3 "$@"
+	set -- "php-fpm${php_version}" "$@"
 fi
 
-if [ "$1" = 'php-fpm7.3' ]; then
+if [ "$1" = "php-fpm${php_version}" ]; then
   mkdir -p /run/php
-  set -- "$@" --nodaemonize --fpm-config /etc/php/7.3/fpm/php-fpm.conf
+  set -- "$@" --nodaemonize --fpm-config "/etc/php/${php_version}/fpm/php-fpm.conf"
 fi
 # On dev servers we add more logging output 
 if [ "$LONG_LIVE" = "n" ]; then
-  printf "display_errors = On\n" > '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "display_startup_errors = On\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "error_reporting = E_ALL\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "html_errors = On\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "log_errors = On\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
+  printf "display_errors = On\n" > "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "display_startup_errors = On\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "error_reporting = E_ALL\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "html_errors = On\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "log_errors = On\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
 fi
 
 exec "$@"
--- etc/dockerfiles/powerbase/docker-entrypoint.sh
+++ etc/dockerfiles/powerbase/docker-entrypoint.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 set -e
 
+php_version=7.4
+
 # If the command passed to docker run/exec starts with a - or -- then
 # set the command.
 if [ "${1:0:1}" = '-' ]; then
-  set -- php-fpm7.3 "$@"
+  set -- "php-fpm${php_version}" "$@"
 fi
 
-if [ "$1" = 'php-fpm7.3' ]; then
+if [ "$1" = "php-fpm${php_version}" ]; then
   mkdir -p /run/php
-  set -- "$@" --nodaemonize --fpm-config /etc/php/7.3/fpm/php-fpm.conf
+  set -- "$@" --nodaemonize --fpm-config "/etc/php/${php_version}/fpm/php-fpm.conf"
 fi
 # On dev servers we add more logging output 
 if [ "$LONG_LIVE" = "n" ]; then
-  printf "display_errors = On\n" > '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "display_startup_errors = On\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "error_reporting = E_ALL\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "html_errors = On\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  printf "log_errors = On\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
+  printf "display_errors = On\n" > "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "display_startup_errors = On\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "error_reporting = E_ALL\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "html_errors = On\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  printf "log_errors = On\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
 
   # Also, we enable xdebug
   # ARG. disabled because it makes everything too slow.
-  #xdebug_file="/etc/php/7.3/mods-available/xdebug.ini"
-  #xdebug_link="/etc/php/7.3/fpm/conf.d/20-xdebug.ini"
+  #xdebug_file="/etc/php/${php_version}/mods-available/xdebug.ini"
+  #xdebug_link="/etc/php/${php_version}/fpm/conf.d/20-xdebug.ini"
   #[ -f "$xdebug_file" ] && [ ! -e "$xdebug_link" ] && ln -s  "$xdebug_link"
 
-  #printf "xdebug.remote_enable=1\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  #printf "xdebug.remote_connect_back=On\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  #printf "xdebug.remote_port=9000\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  #printf "xdebug.profiler_enable=1\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  #printf "xdebug.remote_autostart=true\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  #printf "xdebug.remote_handler=dbgp\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  #printf "xdebug.remote_mode=req\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
-  #printf "xdebug.remote_host=10.11.13.1\n" >> '/etc/php/7.3/fpm/conf.d/99-powerbase-dev.ini'
+  #printf "xdebug.remote_enable=1\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  #printf "xdebug.remote_connect_back=On\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  #printf "xdebug.remote_port=9000\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  #printf "xdebug.profiler_enable=1\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  #printf "xdebug.remote_autostart=true\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  #printf "xdebug.remote_handler=dbgp\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  #printf "xdebug.remote_mode=req\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
+  #printf "xdebug.remote_host=10.11.13.1\n" >> "/etc/php/${php_version}/fpm/conf.d/99-powerbase-dev.ini"
 
   # And we ensure the civix is installed. Sometimes download.civicrm.org is not 
   # available - we don't want to hang forever, so timeout after 30 seconds.
@@ -51,13 +53,7 @@ if [ "$LONG_LIVE" = "n" ]; then
   sed -i "s/bulk\.ourpowerbase\.net/smtp/" /etc/esmtprc
 else
   # Ensure we do not have debugging enabled.
-  rm -f /etc/php/7.3/fpm/conf.d/20-xdebug.ini
-fi
-
-# We use cv to enable extensions.
-if [ ! -f /usr/local/bin/cv ]; then
-  wget --tries=2 --timeout=30 -O /usr/local/bin/cv https://download.civicrm.org/cv/cv.phar
-  chmod 755 /usr/local/bin/cv
+  rm -f "/etc/php/${php_version}/fpm/conf.d/20-xdebug.ini"
 fi
 
 # Allow us to overwrite the DNS caching server at runtime.
